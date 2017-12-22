4eb04584644842cdc2e951ff471666edd7a8cc00
Dipshi <2015dipshi.shetty@ves.ac.in>
2017-12-22 16:27:37 +0530

    Init

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,28 @@
+
+<meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            
+        </div>
+            <div id="navbar" class="collapse navbar-collapse" >
+            
+
+            <ul class="nav navbar-nav">
+              <li><a href="/">Home</a></li>
+              <li><a href="/hi">hey</a></li>
+              <li><a href="/new">new</a></li>
+              <li><a href="/">hello</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
